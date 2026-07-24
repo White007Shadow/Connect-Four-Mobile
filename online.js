b70b7814fcccdff4fac9e3b2ac555915df9a6b73
White007Shadow <beastincorned@gmail.com>
2026-07-25 02:04:41 +0600

    Delete online.js

--- online.js
+++
@@ -1,2 +0,0 @@
-export const firebaseConfig={apiKey:"AIzaSyCZabLqhd02SIXk41FeDm3EtmAmNEhSbTk",authDomain:"connect4-73d21.firebaseapp.com",projectId:"connect4-73d21",storageBucket:"connect4-73d21.firebasestorage.app",messagingSenderId:"959863469334",appId:"1:959863469334:web:87eab2667b0104bed84c37",measurementId:"G-MK1MZCTJJH"};
-export const SITE_URL="https://white007shadow.github.io/Connect4/";
